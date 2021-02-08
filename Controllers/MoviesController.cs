275055257755fb09353ceb96a96ad9b6899b3d71
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2021-02-08 20:51:42 +0530

    Second

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Visly.Models;
+
+namespace Visly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Index
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek !!!", Id = 1 };
+            return View(movie);
+        }
+    }
+}
